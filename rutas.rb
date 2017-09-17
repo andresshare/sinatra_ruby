0a0ef2ca82525b3b328c44b3cc73a1f03ee43f85
andresshare <andresshare@gmail.com>
2017-09-17 15:46:02 -0500

    testing

--- rutas.rb
+++ rutas.rb
@@ -1,6 +1,29 @@
 require "sinatra"
 
 get'/' do
-    "hello"
+    unless params[:nombre]
+        "Hello"    
+    else
+        "Hola #{params[:nombre]}"
+    end
+<<-HTML
+<h1>formulario</h1>
+<form action="nuevo/objeto" method="post">
+<input type="text" name="verbo"></input>
+<input type="submit" value=" valid * POST"> </input>   
+
+</form>
+
+HTML
+
 end
+
+get '/:nombre' do
+"hello!  #{params[:nombre]}"
+end
+
+post'/nuevo/objeto' do
+  "I'm create a new object with verbo #{params[:verbo]}"
+end
+
 
